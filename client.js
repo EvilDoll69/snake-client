89feb961052504f717390c2ceed858db658550be
EvilDoll69 <tetiana.fediushyn@gmail.com>
2021-12-02 03:12:57 +0000

    Some refactoring

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function () { //The conn object that Node returned to you represents the connection that you have with the server.
   const conn = net.createConnection({ //The conn object is full of useful methods and properties that can now be used to interact with the server!
-    host: "165.227.47.243", // IP address here,
-    port: 50541, // PORT number here,
+    host: IP, // IP address here,
+    port: PORT // PORT number here,
     
   });
 
@@ -23,7 +24,7 @@ const connect = function () { //The conn object that Node returned to you repres
 
   conn.on('connect', () => {
     console.log("Success connect to server")
-    conn.write("Name: TF");
+    conn.write(myName);
   });
 
   return conn;
---
+++ constants.js
@@ -0,0 +1,11 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+const myName = "Name: TF";
+
+
+
+
+module.exports = {
+  IP,
+  PORT,
+};
